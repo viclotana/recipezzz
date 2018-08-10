3063a966ed2c9c5d5fb760cdf6eafd946d2c352b
viclotana <viclotana@gmail.com>
2018-08-10 21:44:20 +0100

    hiding my keys

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://viclotana:lll102@ds119422.mlab.com:19422/recipeapp'
+}
--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const app = express();
 
 //body parser middle ware
 app.use(bodyParser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongo db
+mongoose.connect(db)
+.then(() => console.log('Mongo DB Connected'))
+.catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server started 
+on port ${port}`));
