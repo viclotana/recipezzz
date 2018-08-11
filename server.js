3c9b4dd0f634da4a6934e9141f927db77c82283f
viclotana <viclotana@gmail.com>
2018-08-11 21:06:19 +0100

    brought in items

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const items = require('./routes/api/items');
 
 const app = express();
 
@@ -15,6 +16,8 @@ mongoose.connect(db)
 .then(() => console.log('Mongo DB Connected'))
 .catch(err => console.log(err));
 
+// use routes
+app.use('/api/items',items);
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server started 
