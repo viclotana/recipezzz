ad3e6c9dd8bad26f048406dba615956a929d11c6
viclotana <viclotana@gmail.com>
2018-08-11 22:21:13 +0100

    upss

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,4 +13,24 @@ router.get('/', (req, res)=> {
     .then(items => res.json(items));
 });
 
+// @route  POST api/items
+//@desc    Create an item
+//@access  Public
+router.post('/', (req, res)=> {
+    const newItem = new Item({
+        name: req.body.name
+    });
+    newItem.save().then(item => res.json(item));
+});
+
+// @route  DELETE api/items/:id
+//@desc    Delete an item
+//@access  Public
+router.delete('/:id', (req, res)=> {
+   Item.findById(req.params.id)
+   .then(item => item.remove().then(() => res.json({ success: true })))
+   .catch(err => res.status(404).json({ success:false }));
+});
+
+
 module.exports = router;
