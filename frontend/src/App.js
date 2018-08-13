a02b793c62fe05413fd31e88c8ed2efb4c5032c1
viclotana <viclotana@gmail.com>
2018-08-13 19:08:08 +0100

    bringin in recipelist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
-import
+import RecipeList from './components/RecipeList';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
