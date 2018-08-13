8060a5d64ee4d2c3c56e440dd47f19a4de5a2fad
viclotana <viclotana@gmail.com>
2018-08-13 11:21:43 +0100

    upss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+         
         
         </header>
         <p className="App-intro">
