9cbe1470f53fd2baa14a062d718b28c897dee1da
viclotana <viclotana@gmail.com>
2018-08-13 12:27:24 +0100

    upss

--- frontend/src/components/AppNavbar.js
+++ frontend/src/components/AppNavbar.js
@@ -25,7 +25,7 @@ class AppNavbar extends Component {
         <div>
             <Navbar color="dark" dark expand="sm" className="mb-5">
                <Container>
-                   <NavbarBrand href="/"></NavbarBrand>
+                   <NavbarBrand href="/">Recipessss</NavbarBrand>
                </Container>
             </Navbar>
         </div>
