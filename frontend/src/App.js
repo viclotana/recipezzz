52e22731f0f8d63bddee84e31cfc39594272e0b4
viclotana <viclotana@gmail.com>
2018-08-13 11:21:54 +0100

    upss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
          
         
         </header>
-        <p className="App-intro">
+        
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
