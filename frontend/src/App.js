892799254f56283d6699b625d136446fac960ba5
viclotana <viclotana@gmail.com>
2018-08-15 00:51:15 +0100

    bringing in store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import RecipeList from './components/RecipeList';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
