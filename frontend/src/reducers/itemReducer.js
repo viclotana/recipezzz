404a9be7640dfbbe09e2ee232e1ae03eacc354c4
viclotana <viclotana@gmail.com>
2018-08-15 20:01:15 +0100

    upss

--- frontend/src/reducers/itemReducer.js
+++ frontend/src/reducers/itemReducer.js
@@ -1,21 +1,40 @@
 import uuid from 'uuid';
-import { GET_ITEMS, ADD_ITEM,DELETE_ITEM } from '../actions/types';
+import {
+    GET_ITEMS,
+    ADD_ITEM,
+    DELETE_ITEM
+} from '../actions/types';
 
 const initialState = {
-    items: [
-        { id:uuid(), name:'Abacha Ncha', style: 'boil and eat'},
-        { id:uuid(), name:'Oha Soup', style: 'buy but dont boil and eat'},
-        { id:uuid(), name:'Bread and Tea', style: 'buy and eat'},
-        { id:uuid(), name:'Rice and Stew', style: 'buy and eat'}
+    items: [{
+            id: uuid(),
+            name: 'Abacha Ncha',
+            style: 'boil and eat'
+        },
+        {
+            id: uuid(),
+            name: 'Oha Soup',
+            style: 'buy but dont boil and eat'
+        },
+        {
+            id: uuid(),
+            name: 'Bread and Tea',
+            style: 'buy and eat'
+        },
+        {
+            id: uuid(),
+            name: 'Rice and Stew',
+            style: 'buy and eat'
+        }
     ]
 }
 export default (state = initialState, action) => {
-  switch(action.type){
-      case GET_ITEMS:
-      return {
-          ...state
-      }
-      default: 
-      return state;
-  }
+    switch (action.type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            }
+        default:
+            return state;
+    }
 }
