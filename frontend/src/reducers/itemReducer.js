518ea73897bd17646aba9086077375aa461680c3
viclotana <viclotana@gmail.com>
2018-08-15 15:02:25 +0100

    upss

--- frontend/src/reducers/itemReducer.js
+++ frontend/src/reducers/itemReducer.js
@@ -1,1 +1,8 @@
-const initialState = {}
+const initialState = {
+    items: [
+        { id:uuid(), name:'Abacha', style: 'boil and eat'},
+        { id:uuid(), name:'Oha Soup', style: 'buy but dont boil and eat'},
+        { id:uuid(), name:'Bread and Tea', style: 'buy and eat'},
+        { id:uuid(), name:'Rice and Stew', style: 'buy and eat'}
+    ]
+}
